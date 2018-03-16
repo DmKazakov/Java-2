716757415dce34c81f2cf15e4168192605e189e3
Dmitry <qudeed@gmail.com>
2018-03-16 06:38:45 +0300

    Delete extra file

--- ThreadPool/src/main/java/ru/spbau/mit/kazakov/ThreadPool/LightExecutionException.java
+++
@@ -1,7 +0,0 @@
-package ru.spbau.mit.kazakov.ThreadPool;
-
-/**
- * Used to indicate that result of computation can't be obtained because of an exception.
- */
-public class LightExecutionException extends Exception {
-}
